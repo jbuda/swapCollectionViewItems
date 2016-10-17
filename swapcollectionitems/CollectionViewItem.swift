2a83b082523eca674e146d15656f21e2557dc614
janusz <janusz>
2016-10-17 21:32:52 +0100

    Setting project up

---
+++ swapcollectionitems/CollectionViewItem.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewItem.swift
+//  swapcollectionitems
+//
+//  Created by Janusz on 17/10/2016.
+//
+//
+
+import UIKit
+
+class CollectionViewItem: UICollectionViewCell {
+
+  @IBOutlet weak var title:UILabel!
+  
+}
--- swapcollectionitems/ViewController.swift
+++ swapcollectionitems/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var collectionview:UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,22 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+}
 
-
+extension ViewController:UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 16
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "uicollectionviewcell", for: indexPath) as! CollectionViewItem
+    
+    cell.title.text = "\(indexPath.row+1)"
+    
+    return cell
+  }
+  
+  
 }
 
