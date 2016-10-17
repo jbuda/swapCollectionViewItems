f7be2d23a454e1f5ca16c34a180e931d97c452b7
janusz <janusz>
2016-10-17 21:14:18 +0100

    Initial Commit

---
+++ swapcollectionitems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swapcollectionitems
+//
+//  Created by Janusz on 17/10/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
